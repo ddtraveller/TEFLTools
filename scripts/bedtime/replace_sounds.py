8eafaa2597cf1bc595501c7c74974b856d7c8543
Ubuntu <ubuntu@ip-172-31-14-244.us-west-2.compute.internal>
2024-08-19 06:19:00 +0000

    change replace sounds to take an s3 file path instead of using a local file

--- scripts/bedtime/replace_sounds.py
+++ scripts/bedtime/replace_sounds.py
@@ -4,19 +4,22 @@ import re
 from gtts import gTTS
 from io import BytesIO
 import os
+from urllib.parse import urlparse
 
 # Initialize S3 client
 s3 = boto3.client('s3')
 
-def get_bedtime_html():
-    response = s3.get_object(Bucket='tl-web', Key='bedtime.html')
+def get_s3_file(url):
+    parsed_url = urlparse(url)
+    bucket = parsed_url.netloc.split('.')[0]
+    key = parsed_url.path.lstrip('/')
+    response = s3.get_object(Bucket=bucket, Key=key)
     html_content = response['Body'].read().decode('utf-8')
-    return html_content
+    return html_content, bucket, key
 
 def parse_html_for_audio(html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
     audio_data = []
-
     for div in soup.find_all('div', class_='story-part'):
         part_number = div.find('h2').text.strip()
         text = div.find('p').text.strip()
@@ -29,10 +32,9 @@ def parse_html_for_audio(html_content):
                 'text': text,
                 'filename': audio_filename
             })
+    return audio_data, soup
 
-    return audio_data
-
-def generate_sound(text, filename):
+def generate_sound(text, filename, bucket):
     # Replace apostrophes with empty strings
     text = text.replace("'", "")
     text = re.sub(r'Part \d+$', '', text).strip()
@@ -51,28 +53,48 @@ def generate_sound(text, filename):
     
     # Save the audio data to S3
     s3.put_object(
-        Bucket='tl-web',
+        Bucket=bucket,
         Key=f'sounds/{filename}',
         Body=fp,
         ContentType='audio/mpeg'
     )
     
-    return f'https://tl-web.s3.us-west-2.amazonaws.com/sounds/{filename}'
+    return f'https://{bucket}.s3.us-west-2.amazonaws.com/sounds/{filename}'
+
+def update_html(soup, audio_data):
+    for item in audio_data:
+        div = soup.find('div', class_='story-part', string=lambda text: item['part'] in text)
+        if div:
+            audio_tag = div.find('audio')
+            if audio_tag:
+                source_tag = audio_tag.find('source')
+                if source_tag:
+                    source_tag['src'] = f'sounds/{item["filename"]}'
+    return str(soup)
 
 def main():
-    # Get the bedtime.html content
-    html_content = get_bedtime_html()
+    # Prompt for S3 file location
+    s3_url = input("Enter the S3 file URL (e.g., https://tl-web.s3.us-west-2.amazonaws.com/stories/bedtime_1723965739.html): ")
+    
+    # Get the HTML content and S3 details
+    html_content, bucket, key = get_s3_file(s3_url)
     
     # Parse the HTML to extract audio data
-    audio_data = parse_html_for_audio(html_content)
+    audio_data, soup = parse_html_for_audio(html_content)
     
     # Generate and replace sound files
     for item in audio_data:
         print(f"Generating new audio for {item['part']}")
-        new_audio_url = generate_sound(item['text'], item['filename'])
+        new_audio_url = generate_sound(item['text'], item['filename'], bucket)
         print(f"New audio file generated and uploaded: {new_audio_url}")
-
-    print("All sound files have been replaced successfully.")
+    
+    # Update the HTML content
+    updated_html = update_html(soup, audio_data)
+    
+    # Upload the updated HTML back to S3
+    s3.put_object(Bucket=bucket, Key=key, Body=updated_html, ContentType='text/html')
+    
+    print(f"Updated HTML file has been uploaded back to S3: {s3_url}")
 
 if __name__ == "__main__":
     main()
